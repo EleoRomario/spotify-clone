acf4b048a81d393594ae2ac6d25682e9fcc37195
EleoRomario <eleo.romario@gmail.com>
2023-11-14 17:52:05 -0500

    app sidebar

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				black: {
+					DEFAULT: "#121212",
+					400: "#2a2a2a",
+					500: "#1a1a1a",
+				},
+				text: "#a7a7a7",
+			},
+		},
+		boxShadow: {
+			side: "0 6px 10px rgba(0,0,0,.6)",
+		},
+	},
+	plugins: [],
+};
